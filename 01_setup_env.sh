de2df9f6a0c96643b918cc642df013c16fe07821
Gabriele Ferroni <gabri.00ferroni@outlook.it>
2026-02-12 11:05:26 +0100

    Enhance setup script with new software and miRBase download
    Updated the setup script to include additional software installations and improved the download process for miRBase files.
    we downloaded 2 file: one about mature miRNA and another about hairpins. then, we merged together and filtered by Homo Sapiens. 
    set all folders we need and the variables of "for loops".

--- 01_setup_env.sh
+++ 01_setup_env.sh
@@ -1,25 +1,76 @@
-#!/bin/bash
-
-# SETUP AMBIENTE DI LAVORO
+###################################
+# CREAZIONE AMBIENTE CONDA
 ###################################
 
-# AMBIENTE CONDA APPOSITO
 conda create -n mirna_qiaseq -y
 conda activate mirna_qiaseq
 
-# INSTALLAZIONE DEI SOFTWARE NECESSARI
-conda install -y -c bioconda fastqc cutadapt bowtie samtools umi_tools
+###################################
+# INSTALLAZIONE SOFTWARE
+###################################
+
+conda install -y -c bioconda \
+  fastqc \
+  cutadapt \
+  bowtie \
+  samtools \
+  umi_tools \
+  subread
+
 conda install -y -c conda-forge pigz
 
+###################################
+# R + PACCHETTI BIOC
+###################################
+
 R -e 'install.packages("BiocManager")'
-R -e 'BiocManager::install(c("edgeR","limma","pheatmap","EnhancedVolcano","clusterProfiler","org.Hs.eg.db"))'
+R -e 'BiocManager::install(c("edgeR","limma","pheatmap","EnhancedVolcano","UpSetR","clusterProfiler","org.Hs.eg.db"))'
 
-# DOWNLOAD DEI FILE DI RIFERIMENTO miRNA
-wget https://mirbase.org/download/hairpin.fa.gz
-wget https://mirbase.org/download/mature.fa.gz
-gunzip *.gz
-grep -A1 "hsa-" mature.fa > hsa_mature.fa
-grep -A1 "hsa-" hairpin.fa > hsa_hairpin.fa
+###################################
+# DOWNLOAD miRBase
+###################################
 
-# INDICIZZAZIONE DEI FILE DI RIFERIMENTO CON BOWTIE
-bowtie-build hsa_mature.fa hsa_mature
+# reference miRNA (mirBASE)
+
+wget https://mirbase.org/download/mature.fa
+wget https://mirbase.org/download/hairpins.fa
+
+# https://www.mirbase.org/download/ #
+
+cat mature.fa hairpins.fa > hsa_mature.fa          # concateno i 2 file (miRNA maturi e hairpins)
+
+# solo Homo sapiens
+grep -A1 "^>hsa-" hsa_mature.fa > hsa_miRNA.fa
+
+###################################
+# INDICIZZAZIONE BOWTIE
+###################################
+
+bowtie-build hsa_miRNA.fa hsa_miRNA
+
+###################################
+###################################
+# VARIABILI
+###################################
+
+THREADS=16
+
+RAW=raw_data
+TRIM=trimmed
+ALIGN=alignment
+COUNT=counts
+LOGS=logs
+
+REF_IDX=/home/user/reference/hsa_miRNA
+
+###################################
+##### ADAPTER QIAseq miRNA #####
+##### VERIFICA SUL MANUALE #####
+##### E MODIFICA QUI !!!!! #####
+###################################
+
+ADAPTER="INSERISCI_QUI_ADAPTER_QIASEQ_CORRETTO" ################################################################################
+
+mkdir -p fastqc $TRIM $ALIGN $COUNT $LOGS
+
+fastqc -o fastqc -t $THREADS $RAW/*.fastq.gz
